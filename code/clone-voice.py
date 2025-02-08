bc83203482a10b3a92afd5fb84b8f81ac1b795d6
jims57 <jims57@163.com>
2025-02-08 17:14:36 +0800

    1

--- code/clone-voice.py
+++ code/clone-voice.py
@@ -4,6 +4,15 @@ from cosyvoice.cli.cosyvoice import CosyVoice, CosyVoice2
 from cosyvoice.utils.file_utils import load_wav
 import torchaudio
 import torch
+import gc
+
+torch.cuda.empty_cache()
+
+# Force garbage collection
+gc.collect()
+
+# Check available memory
+print(f"Available GPU memory: {torch.cuda.get_device_properties(0).total_memory / 1024**2:.0f}MB")
 
 print(f"PyTorch version: {torch.__version__}")
 print(f"CUDA available: {torch.cuda.is_available()}")
@@ -22,6 +31,13 @@ else:
 
 print(f"==== Using device: {device} ====")
 
+# Print GPU info
+print(f"Using GPU: {torch.cuda.get_device_name(0)}")
+print(f"GPU Memory Usage:")
+print(f"  Allocated: {torch.cuda.memory_allocated(0)/1024**2:.2f} MB")
+print(f"  Cached:    {torch.cuda.memory_reserved(0)/1024**2:.2f} MB")
+
+# cosyvoice = CosyVoice2('pretrained_models/CosyVoice2-0.5B', load_jit=False, load_trt=False, fp16=False)
 cosyvoice = CosyVoice2('pretrained_models/CosyVoice2-0.5B', load_jit=False, load_trt=False, fp16=False)
 
 # Clone voice[小猪配奇]
@@ -30,6 +46,11 @@ cosyvoice = CosyVoice2('pretrained_models/CosyVoice2-0.5B', load_jit=False, load
 #     torchaudio.save('zero_shot_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
 
 # Clone voice[哆啦A梦]
-prompt_speech_16k = load_wav('./audios/哆啦A梦1.mp3', 16000)
-for i, j in enumerate(cosyvoice.inference_zero_shot('他在旧书店找到一本泛黄的笔记本，封面上写着一个熟悉的名字。', '地址就是这里没有啊，你看，云遊旅馆的招牌就挂在那里，没有吧？', prompt_speech_16k, stream=False)):
+# prompt_speech_16k = load_wav('./audios/哆啦A梦1.mp3', 16000)
+# for i, j in enumerate(cosyvoice.inference_zero_shot('他在旧书店找到一本泛黄的笔记本，封面上写着一个熟悉的名字。', '地址就是这里没有啊，你看，云遊旅馆的招牌就挂在那里，没有吧？', prompt_speech_16k, stream=False)):
+#     torchaudio.save('zero_shot_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
+
+# Clone voice[哆啦A梦]
+prompt_speech_16k = load_wav('./audios/fd-3.MP3', 16000)
+for i, j in enumerate(cosyvoice.inference_zero_shot('他在旧书店找到一本泛黄的笔记本，封面上写着一个熟悉的名字。', '想来想去，不要，这份工作我不要了。就就，打算拒绝这份工作。后来这个作者听说这种事以后就特别担心。', prompt_speech_16k, stream=False)):
     torchaudio.save('zero_shot_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
