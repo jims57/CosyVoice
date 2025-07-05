4455d9b5f32e3543569eabf7d83874902b116874
jims57 <jims57@163.com>
2025-07-05 10:14:12 +0800

    ok

--- my-info/utils/push_image_to_acr.sh
+++ my-info/utils/push_image_to_acr.sh
@@ -1,10 +1,5 @@
-# docker login --username=jimmy@1904289922758417 crpi-e59jvk2aewsjh4xc.cn-shanghai.personal.cr.aliyuncs.com
+# save container to image
+version="1.3.1" && server_name="cosy-tts" && registry_url="crpi-e59jvk2aewsjh4xc.cn-shanghai.personal.cr.aliyuncs.com/watchfun-fc3-space" && image_name="${registry_url}/${server_name}" && tag="v${version}" && docker commit --pause=false "${server_name}" "${image_name}:${tag}"
 
-docker tag jims57/cosy-tts:v1.3 crpi-e59jvk2aewsjh4xc.cn-shanghai.personal.cr.aliyuncs.com/watchfun-fc3-space/cosy-tts:v1.3
-docker push crpi-e59jvk2aewsjh4xc.cn-shanghai.personal.cr.aliyuncs.com/watchfun-fc3-space/cosy-tts:v1.3
-
-# docker pull crpi-e59jvk2aewsjh4xc.cn-shanghai.personal.cr.aliyuncs.com/watchfun-fc3-space/coqui-tts:v1.3
-
-# version="1.2.1" && server_name="cosy-tts" && image_prefix="crpi-e59jvk2aewsjh4xc.cn-shanghai.personal.cr.aliyuncs.com/watchfun-fc3-space" && image_name="${image_prefix}/${server_name}" && tag="v${version}" && docker rm "${server_name}" -f && docker run --gpus all -it -p 9000:9000 -p 9002:9002 -p 9003:9003 --shm-size=24g -w ~/TTS --name "${server_name}" "${image_name}:${tag}" /bin/bash -c "bash /root/.jupyter_startup.sh; /bin/bash"
-
-# version="1.2.1" && server_name="cosy-tts" && image_prefix="crpi-e59jvk2aewsjh4xc.cn-shanghai.personal.cr.aliyuncs.com/watchfun-fc3-space" && image_name="${image_prefix}/${server_name}" && tag="v${version}" && docker run --gpus all -it -p 9000:9000 -p 9002:9002 -p 9003:9003 --shm-size=24g -w ~/TTS --name "${server_name}" "${image_name}:${tag}" /bin/bash -c "bash /root/.jupyter_startup.sh; /bin/bash"
+# push image to acr
+docker push "${registry_url}/cosy-tts:v${version}"
